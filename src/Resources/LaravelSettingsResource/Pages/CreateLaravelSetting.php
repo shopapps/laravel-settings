e5806e92cbcd4cf212e2257433242f788ae2bc68
Paul <paul@shopapps.co.uk>
2024-12-29 15:07:59 +0000

    Update CreateLaravelSetting.php

--- src/Resources/LaravelSettingsResource/Pages/CreateLaravelSetting.php
+++ src/Resources/LaravelSettingsResource/Pages/CreateLaravelSetting.php
@@ -2,19 +2,19 @@
 
 namespace Shopapps\LaravelSettings\Resources\LaravelSettingsResource\Pages;
 
-use Shopapps\LaravelSettings\Resources\PermissionResource;
+use Shopapps\LaravelSettings\Resources\LaravelSettingsResource;
 use Shopapps\LaravelSettings\Resources\RoleResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateLaravelSetting extends CreateRecord
 {
-    protected static string $resource = PermissionResource::class;
+    protected static string $resource = LaravelSettingsResource::class;
 
     protected function getRedirectUrl(): string
     {
         $resource = static::getResource();
-
-        return config('filament-spatie-roles-permissions.should_redirect_to_index.permissions.after_create', false)
+        
+        return config('laravel-settings.should_redirect_to_index.settings.after_create', false)
             ? $resource::getUrl('index')
             : parent::getRedirectUrl();
     }
