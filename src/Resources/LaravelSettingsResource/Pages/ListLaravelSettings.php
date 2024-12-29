682603a9c00234ce69105d3087ed0c6bd8051f47
Paul <paul@shopapps.co.uk>
2024-12-29 15:04:04 +0000

    Update ListLaravelSettings.php

--- src/Resources/LaravelSettingsResource/Pages/ListLaravelSettings.php
+++ src/Resources/LaravelSettingsResource/Pages/ListLaravelSettings.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Shopapps\LaravelSettings\Resources\PermissionResource\Pages;
+namespace Shopapps\LaravelSettings\Resources\LaravelSettingsResource\Pages;
 
 use Shopapps\LaravelSettings\Resources\LaravelSettingsResource;
 use Filament\Actions\CreateAction;
