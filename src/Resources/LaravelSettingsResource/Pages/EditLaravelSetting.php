5149017b83473eca83bd62867d2fd0080e2e2338
Paul Robbins <paul@shopapps.co.uk>
2024-12-30 15:25:20 +0000

    Add type-based data mutation for settings create/edit pages
    Introduce `mutateFormDataBeforeCreate` and `mutateFormDataBeforeSave` methods to handle type-specific data transformations. This ensures proper data formatting and storage by converting values to their respective types based on the `type` field.

--- src/Resources/LaravelSettingsResource/Pages/EditLaravelSetting.php
+++ src/Resources/LaravelSettingsResource/Pages/EditLaravelSetting.php
@@ -4,6 +4,7 @@ namespace Shopapps\LaravelSettings\Resources\LaravelSettingsResource\Pages;
 
 use Shopapps\LaravelSettings\Resources\LaravelSettingsResource;
 use Filament\Resources\Pages\EditRecord;
+use App\Models\LaravelSetting as LaravelSettingModel;
 
 class EditLaravelSetting extends EditRecord
 {
@@ -17,4 +18,35 @@ class EditLaravelSetting extends EditRecord
             ? $resource::getUrl('index')
             : parent::getRedirectUrl();
     }
+
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        switch(data_get($data, 'type')) {
+            case LaravelSettingModel::TYPE_BOOLEAN:
+                $data['value'] = (bool) $data['value'];
+                break;
+            case LaravelSettingModel::TYPE_INTEGER:
+                $data['value'] = (int) $data['value'];
+                break;
+            case LaravelSettingModel::TYPE_FLOAT:
+                $data['value'] = (float) $data['value'];
+                break;
+            case LaravelSettingModel::TYPE_ARRAY:
+                // starts as a comma delimited list, explode and trim
+                $data['value'] = array_map('trim', explode(',', $data['value']));
+                $data['value'] =  json_encode($data['value']);
+                break;
+            case LaravelSettingModel::TYPE_OBJECT:
+                $data['value'] = array_map('trim', explode(',', $data['value']));
+                $data['value'] = (object) json_encode($data['value']);
+                break;
+            case LaravelSettingModel::TYPE_STRING:
+            default:
+                $data['value'] = (string) $data['value'];
+                break;
+        }
+        return $data;
+    }
+
 }
